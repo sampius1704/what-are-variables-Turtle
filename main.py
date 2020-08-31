ceed4125da44671ad6bbaf5f3a8bc095fae65f62
Samuel Pius <sampius1704@gmail.com>
2020-08-31 04:40:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+a = 17
+print(a)
+
+sam_turtle = turtle.Turtle()
+
+def square():
+  sam_turtle.forward(100)
+  sam_turtle.right(90)
+  sam_turtle.forward(100)
+  sam_turtle.right(90)
+  sam_turtle.forward(100)
+  sam_turtle.right(90)
+  sam_turtle.forward(100)
+  
+
+square()
+sam_turtle.forward(100)
+square()
